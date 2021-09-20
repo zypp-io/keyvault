3e7ca21f6f77c69ad01632f383ce3b29851c615a
Melvin Folkers <melvin@yellowstacks.nl>
2021-09-20 18:24:14 +0200

    added delete_keyvault_secrets

--- keyvault/__init__.py
+++ keyvault/__init__.py
@@ -1,11 +1,12 @@
-from keyvault.get_secrets import secrets_to_environment
-from keyvault.utils import get_dotenv_secrets
-from keyvault.get_secrets import get_keyvault_secrets
-from keyvault.utils import dotenv_to_keyvault
-from keyvault.utils import dict_to_keyvault
-
-
 import logging
+
+from keyvault.get_secrets import get_keyvault_secrets, secrets_to_environment
+from keyvault.utils import (
+    delete_keyvault_secrets,
+    dict_to_keyvault,
+    dotenv_to_keyvault,
+    get_dotenv_secrets,
+)
 
 logging.getLogger("azure.core.pipeline.policies.http_logging_policy").setLevel(logging.WARNING)
 logging.getLogger("azure.identity").setLevel(logging.WARNING)
