281e722293474c779df0501a878279f9dc9af12e
Tim van der Heijden <tim@zypp.io>
2024-05-31 15:00:45 +0200

    Update imports

--- keyvault/__init__.py
+++ keyvault/__init__.py
@@ -1,11 +1,14 @@
 import logging
 
-from keyvault.get_secrets import get_keyvault_secrets, secrets_to_environment
+from keyvault.get_secrets import (
+    get_keyvault_secrets as get_keyvault_secrets,
+    secrets_to_environment as secrets_to_environment,
+)
 from keyvault.utils import (
-    delete_keyvault_secrets,
-    dict_to_keyvault,
-    dotenv_to_keyvault,
-    get_dotenv_secrets,
+    delete_keyvault_secrets as delete_keyvault_secrets,
+    dict_to_keyvault as dict_to_keyvault,
+    dotenv_to_keyvault as dotenv_to_keyvault,
+    get_dotenv_secrets as get_dotenv_secrets,
 )
 
 logging.getLogger("azure.core.pipeline.policies.http_logging_policy").setLevel(logging.WARNING)
